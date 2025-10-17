be2e8edf0966a6696fdf57e8a52cc5750e8604bb
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-10-17 14:57:41 +0530

    Create fibonacii.java

---
+++ fibonacii.java
@@ -0,0 +1,14 @@
+package _5_Recursion;
+
+public class Fibonacci {
+    public static  int fibo(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fibo(n));
+    }
+}
